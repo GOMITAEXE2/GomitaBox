c0950f1fd20ef666969acbac436cfc371d4e5d02
GOMITAEXE2 <agustinleonardo433@gmail.com>
2024-10-31 17:38:10 -0300

    Nuevos cambios

--- Assets/Scripts/MovimientoDeAutito.cs
+++ Assets/Scripts/MovimientoDeAutito.cs
@@ -16,34 +16,30 @@ public class MovimientoDeAutito : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //DRAG
-        FuerzaMovimiento *= Drag;
-        FuerzaMovimiento = Vector3.ClampMagnitude(FuerzaMovimiento, VelocidadMaxima);
 
-        //Direccion
-        float steerInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * steerInput * FuerzaMovimiento.magnitude * AnguloDireccion * Time.deltaTime);
-
-        Debug.DrawRay(transform.position, FuerzaMovimiento * 3);
-        Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
-
-    }
-
-    public void Aceleracion(InputAction.CallbackContext context)
-    {
         //MOVIMIENTO
-        if (context.performed)
-        {
+        
             FuerzaMovimiento += transform.forward * VelocidadMovimiento * Input.GetAxis("Vertical") * Time.deltaTime;
             transform.position += FuerzaMovimiento * Time.deltaTime;
 
             //TRACCION
-           
             FuerzaMovimiento = Vector3.Lerp(FuerzaMovimiento.normalized, transform.forward, Traccion * Time.deltaTime) * FuerzaMovimiento.magnitude;
 
-        }
+            //DRAG
+            FuerzaMovimiento *= Drag;
+            FuerzaMovimiento = Vector3.ClampMagnitude(FuerzaMovimiento, VelocidadMaxima);
+
+            //Direccion
+            float steerInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up * steerInput * FuerzaMovimiento.magnitude * AnguloDireccion * Time.deltaTime);
+
+            Debug.DrawRay(transform.position, FuerzaMovimiento * 3);
+            Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
 
     }
+    //public void aceleracion(inputaction.callbackcontext context)
+    //{
+        
 
 
 }
